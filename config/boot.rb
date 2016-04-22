7096c05968ef4fc3ee5dc5c3b088af39a429b682
Tim Perkins <tperkins@salsify.com>
2016-04-22 12:01:08 -0400

    Specify default port

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require 'rails/commands/server'
+
+module Rails
+  class Server
+    def default_options
+      super.merge(Port: 21000)
+    end
+  end
+end
