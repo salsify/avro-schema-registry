95c085e5f022ffa44834e68415ba0cab6dd3854c
Tim Perkins <tperkins@salsify.com>
2016-06-10 08:26:38 -0400

    PR feedback

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,12 @@ class PagesController < ApplicationController
   # This page is primarily for the benefit of the schema registry hosted
   # at avro-schema-registry.salsify.com
   def index
-    render file: "#{Rails.root}/public/index"
+    render(file: "#{Rails.root}/public/index")
   end
 
   # This page is displayed after successfully deploying the app using the
   # Heroku button.
   def success
-    render file: "#{Rails.root}/public/success"
+    render(file: "#{Rails.root}/public/success")
   end
 end
