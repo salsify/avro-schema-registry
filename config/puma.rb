455fcbbc16d84ab865888696330c90c3fd1694b8
Tim Perkins <tperkins@salsify.com>
2016-06-20 12:11:06 -0400

    Standard puma config variables and port configuration

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,8 @@
 # Simple configuration based on
 # https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
 
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+workers Integer(ENV['WEB_WORKER_PROCESSES'] || 2)
+threads_count = Integer(ENV['MAX_WEB_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
