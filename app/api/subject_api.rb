1725edc2901de1ec9a28fc811141ef27d2f2733c
Tim Perkins <tperkins@salsify.com>
2017-01-12 12:38:39 -0500

    Add surrogate control header

--- app/api/subject_api.rb
+++ app/api/subject_api.rb
@@ -58,6 +58,7 @@ class SubjectAPI < Grape::API
     post '/versions' do
       schema = Schemas::RegisterNewVersion.call(params[:name], params[:schema])
       status 200
+      header('Surrogate-Control', 'max-age=2592000')
       header('Cache-Control', 'public, max-age=2592000') # TODO: constants
       { id: schema.id }
     end
