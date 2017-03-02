37922d3ef77d749561012453401385d598310141
Tim Perkins <tjwp@users.noreply.github.com>
2017-03-02 15:19:33 -0500

    Update schema.rb for Rails 5.0 (#34)

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -18,36 +17,32 @@ ActiveRecord::Schema.define(version: 20160416174131) do
 
   create_table "configs", id: :bigserial, force: :cascade do |t|
     t.string   "compatibility"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.integer  "subject_id",    limit: 8
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.bigint   "subject_id"
+    t.index ["subject_id"], name: "index_configs_on_subject_id", unique: true, using: :btree
   end
-
-  add_index "configs", ["subject_id"], name: "index_configs_on_subject_id", unique: true, using: :btree
 
   create_table "schema_versions", id: :bigserial, force: :cascade do |t|
-    t.integer "version",              default: 1
-    t.integer "subject_id", limit: 8,             null: false
-    t.integer "schema_id",  limit: 8,             null: false
+    t.integer "version",    default: 1
+    t.bigint  "subject_id",             null: false
+    t.bigint  "schema_id",              null: false
+    t.index ["subject_id", "version"], name: "index_schema_versions_on_subject_id_and_version", unique: true, using: :btree
   end
-
-  add_index "schema_versions", ["subject_id", "version"], name: "index_schema_versions_on_subject_id_and_version", unique: true, using: :btree
 
   create_table "schemas", id: :bigserial, force: :cascade do |t|
     t.string   "fingerprint", null: false
     t.text     "json",        null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["fingerprint"], name: "index_schemas_on_fingerprint", unique: true, using: :btree
   end
-
-  add_index "schemas", ["fingerprint"], name: "index_schemas_on_fingerprint", unique: true, using: :btree
 
   create_table "subjects", id: :bigserial, force: :cascade do |t|
     t.text     "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_subjects_on_name", unique: true, using: :btree
   end
-
-  add_index "subjects", ["name"], name: "index_subjects_on_name", unique: true, using: :btree
 
 end
