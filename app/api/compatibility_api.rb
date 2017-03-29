a49f8dfaa4a10f7104f40be5544a06dc5602446d
Tim Perkins <tperkins@salsify.com>
2017-03-29 14:36:59 -0400

    Allow level to be specified when checking compatibility

--- app/api/compatibility_api.rb
+++ app/api/compatibility_api.rb
@@ -7,6 +7,14 @@ class CompatibilityAPI < Grape::API
     invalid_avro_schema!
   end
 
+  rescue_from Grape::Exceptions::ValidationErrors do |e|
+    if e.errors.keys == [%w(with_compatibility)]
+      invalid_compatibility_level!
+    else
+      error!({ message: e.message }, 422)
+    end
+  end
+
   rescue_from :all do
     server_error!
   end
@@ -18,11 +26,17 @@ class CompatibilityAPI < Grape::API
     requires :version_id, types: [Integer, String],
              desc: 'Version of the schema registered under the subject'
     requires :schema, type: String, desc: 'New Avro schema to compare against'
+    optional :with_compatibility, type: String, desc: 'The compatibility level to test',
+             values: Compatibility::Constants::VALUES.to_a
   end
   post '/subjects/:subject/versions/:version_id', requirements: { subject: Subject::NAME_REGEXP } do
     with_schema_version(params[:subject], params[:version_id]) do |schema_version|
       status 200
-      { is_compatible: SchemaRegistry.compatible?(params[:schema], version: schema_version) }
+      {
+        is_compatible: SchemaRegistry.compatible?(params[:schema],
+                                                  version: schema_version,
+                                                  compatibility: params[:with_compatibility])
+      }
     end
   end
 end
