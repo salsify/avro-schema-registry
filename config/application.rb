331499d5b2ab09d3fe038dace7b4ef713024d023
Tim Perkins <tim.perkins@ezcater.com>
2017-12-07 12:13:54 -0500

    PR feedback
    Also had to remove config references to `action_mailer` and
    `assets` after cleaning up railtie requires.

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
