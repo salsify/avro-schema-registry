a9beb86a3355bfe8ade4d25afe1261a58898ba00
Tim Perkins <tim.perkins@ezcater.com>
2017-12-07 09:59:59 -0500

    rails app:update:configs and edits

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-require 'rails'
-# Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_view/railtie'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
