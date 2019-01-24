7345a402a9a2de89df4e6d1360eb03e8e09681e4
Michael Berkowitz <michael.berkowitz@gmail.com>
2019-01-24 10:23:08 -0500

    Add SSL options to disable for redirects so that kube-internal communication can work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,11 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
+  config.ssl_options = {
+    redirect: false,
+    hsts: false,
+    secure_cookies: true
+  }
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
