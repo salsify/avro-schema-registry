05a5f2f7e4d24b1d73f1a1e83f8f78904d648b6e
Josh Branham <josh.php@gmail.com>
2019-09-13 13:27:10 -0400

    Set log level staging (#178)

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in
   # config/environments/production.rb.
 
-  config.log_level = :debug
+  config.log_level = :info
 
   config.x.default_compatibility = ENV.fetch('DEFAULT_COMPATIBILITY', 'NONE')
 end
