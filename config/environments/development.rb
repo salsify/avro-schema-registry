2468633c782659898008f1e7d3ee9c61aae59fb5
Jasper <jasper.chen@ezcater.com>
2020-10-06 11:07:49 -0400

    Set authorized hosts to nil for local development (#112)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  config.hosts = nil
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
