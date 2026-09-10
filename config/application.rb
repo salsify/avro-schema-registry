3b6f68db99e8c0e1118411756f0d83a0cc3edd23
Erik Kessler <ekessler@salsify.com>
2026-09-10 10:21:13 -0400

    [PIMCORE-5684] Update to Rails 8.1

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ Bundler.require(*Rails.groups)
 
 module AvroSchemaRegistry
   class Application < Rails::Application
-    config.load_defaults 8.0
+    config.load_defaults 8.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
